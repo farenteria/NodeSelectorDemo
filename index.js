6ad8873e4024beea51c22e73870f4158bce1d4e3
Fernando Renteria <rentfern@gmail.com>
2018-11-15 16:11:04 -0600

    Read from country file and display each country as a separate anchor on page

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 // modules
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // globals
 const PORT = process.env.PORT || 8081;
 const app = express();
+let countries = [];
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 
+// File Reading
+function getLocations(){
+    fs.readFile("./public/Countries.txt", {encoding: "utf-8"}, (err, data) => {
+        if(!err){
+            // Only one location per line in text file!
+            countries = data.split("\n");
+        }
+    });
+}
+
 // GET ROUTES
 app.get("/", (req, res) => {
-    res.render("index");
+    getLocations();
+    res.render("index", {countries: countries});
 });
 
 // Server initialization
