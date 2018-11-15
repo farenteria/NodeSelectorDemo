dd894e008f7770d2161cb3d90320c1b0559d70e4
Fernando Renteria <rentfern@gmail.com>
2018-11-15 14:15:40 -0600

    Added ejs and view engine for ejs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+// modules
 const express = require("express");
+const path = require("path");
+
+// globals
+const PORT = process.env.PORT || 8081;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
+
+// GET ROUTES
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Server initialization
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}`);
+});
