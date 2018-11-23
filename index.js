8755114208881ca2ae7c234e364d05676793a5d4
Fernando Renteria <rentfern@gmail.com>
2018-11-22 19:34:46 -0600

    Added temporary jquery to fix ajax calls. Remember to remove later. Successful GET request

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 // globals
 const PORT = process.env.PORT || 8081;
 const app = express();
 let countries = [];
+let states = [];
+let cities = [];
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// Process application/json
+// app.use(bodyParser.json());
 
 // File Reading
 function getLocations(){
@@ -19,16 +26,25 @@ function getLocations(){
             countries = data.split("\n");
         }
     });
+
+    fs.readFile("./public/CanadaStates.txt", {encoding: "utf-8"}, (err, data) => {
+        if(!err){
+            // Only one location per line in text file!
+            states = data.split("\n");
+        }
+    });
 }
 
 // ROUTES
 app.get("/", (req, res) => {
     getLocations();
-    res.render("index", {countries: countries});
+    console.log('From get, req.body is:', req.body);
+    res.render("index", {countries: countries, states: states, cities: cities});
 });
 
-app.post("/", (req, res) => {
-    console.log(req.body);
+app.get("/:location", (req, res) => {
+    console.log("/:location get", req.params.location);
+    res.send("here you go");
 });
 
 // Server initialization
--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,4 +1,11 @@
 let countSel = document.getElementById("count-select");
+let dropdownForm = document.getElementById("location-form");
+let dropButton = document.getElementById("sub-but");
+
+dropButton.addEventListener("click", (event) => {
+    // event.preventDefault();
+});
+
 
 // will listen for a change within the dropdown menu
 document.addEventListener("DOMContentLoaded", function(){
@@ -7,4 +14,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function changeEventHandler(event){
     console.log(event.target.value);
+
+    // let oReq = new XMLHttpRequest();
+    // oReq.open("GET", 'localhost:8081/data');
+    // oReq.send(event.target.value);
+    $.get(`/data:${event.target.value}`, function(data, status){
+        console.log("sent", data, status);
+    });
+}
+
+function exListener(){
+    // console.log(this.responseText);
 }
